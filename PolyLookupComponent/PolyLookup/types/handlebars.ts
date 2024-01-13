6367e005197b886bb475160a6c8682863ad575cc
Khoa Nguyen <nhnkhoa@live.com>
2024-01-13 22:42:01 +0000

    PolyLookup/91-cache-not-invalidated (#92)
    * use filter query for empty search
    
    * upgrade prettier

--- PolyLookupComponent/PolyLookup/types/handlebars.ts
+++ PolyLookupComponent/PolyLookup/types/handlebars.ts
@@ -1,5 +1,20 @@
-declare module "handlebars/lib/handlebars" {
-  // Re-export the types from the existing handlebars module
-  import Handlebars = require("handlebars");
-  export default Handlebars;
-}
+import Handlebars from "handlebars";
+
+export const getHBVars = (value: string) => {
+  const ast = Handlebars.parse(value);
+  const keys = [];
+
+  for (const i in ast.body) {
+    if (ast.body[i].type === "MustacheStatement") {
+      const statement = ast.body[i] as hbs.AST.MustacheStatement;
+      if (statement.path.type === "PathExpression") {
+        const varName = (statement.path as hbs.AST.PathExpression).original;
+        const attr = varName.split(".").at(0);
+        if (attr) {
+          keys.push(attr);
+        }
+      }
+    }
+  }
+  return Array.from(new Set<string>(keys));
+};
